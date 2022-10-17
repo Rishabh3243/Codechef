c2c47a3fbe4d94ce1fe5ab73331a3d4662b2e81f
Rishabh Parmar <95970520+Rishabh3243@users.noreply.github.com>
2022-10-17 12:40:00 +0530

    yu

--- src/Javascript/main.js
+++ src/Javascript/main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from './home';
 import About from './about';
+import Team from './Team';
 
 function Main() {
     return (
       <div>
         <Home />
         <About />
+        <Team />
       </div>
     );
   }
