e341e170773b944d74a3f648d92863ade146c312
Rishabh Bharatbhai Parmar <rishabhpar7@gmail.com>
2022-10-30 17:07:02 +0530

    thr

--- src/Javascript/footer.js
+++ src/Javascript/footer.js
@@ -2,19 +2,21 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Main css/footer.css';
 import lof from '../Images/CodeChef_BVM_Chapter-logo-2.png';
+import {BsLinkedin,BsDiscord,BsInstagram,BsArrowReturnRight} from "react-icons/bs";
 function Footer(){
+    <script src="https://kit.fontawesome.com/879cf924f2.js"></script>
     return(
         <div id="footer123">
         <div class="row">
             <div class="col">
                 <div className="Footer12344">
                     <img src={lof} className="Footer1234"/>
-                    <h5>Codechef BVM Chapter</h5>
+                    <h4>Codechef BVM Chapter</h4>
                 </div>
                 <div className="Footer12345">
-                    <h6>
+                    <h5>
                        About: - 
-                    </h6>
+                    </h5>
                     <p>
                     CodeChef College Chapters are the programming clubs run and maintained by the official 
                     Chapter Leaders and mentored by CodeChef. We aim to make 
@@ -24,12 +26,22 @@ function Footer(){
                 </div>
             </div>
             <div class="col">
-                <div>
-                    <h6> Links </h6> 
+                <div className="Footer98">
+                    <h5> Links </h5> 
+                    <p>
+                        <BsArrowReturnRight className="icon12" size={26}/>
+                    </p>
                 </div>
             </div>
             <div class="col">
-            Column
+                <div className="Footer99">
+                    <h5>Follow Us</h5>
+                    <p>
+                    <a href="https://www.linkedin.com/in/codechef-bvm-chapter/"><BsLinkedin className="icon12" size={36}/></a>
+                    <a href=""><BsDiscord className="icon12" size={36}/></a>
+                    <a href=""><BsInstagram className="icon12" size={36}/></a>
+                    </p>
+                </div>
             </div>
         </div>
         </div>
